4da9ebbc12501e7f1d316dd5dad105625ea22a8b
Ian A. Ruiz <ian.alfonso.ruiz.naranjo@gmail.com>
2021-05-03 12:53:22 -0700

    Agregar resultados de U3T4.

--- semester-1/vision-artificial/U3T4/U3T4_1.m
+++ semester-1/vision-artificial/U3T4/U3T4_1.m
@@ -3,7 +3,7 @@ image = rgb2gray(imread('./images/camaleon_puntos.png'));
 
 window = [-1 -1 -1; -1 8 -1; -1 -1 -1];
 resulting_image = abs(imfilter(double(image), window));
-T = 200;
+T = 150;
 resulting_image = resulting_image >= T;
 
 figure();
