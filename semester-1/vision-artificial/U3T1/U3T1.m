2f88659394cb6bc9efb9e85840d3e83855942167
Ian A. Ruiz <ian.alfonso.ruiz.naranjo@gmail.com>
2021-05-02 18:03:13 -0700

    Agregar títulos a imagenes resultado después de detección de bordes.
    - Agregar imágenes de resultado como referencia.

--- semester-1/vision-artificial/U3T1/U3T1.m
+++ semester-1/vision-artificial/U3T1/U3T1.m
@@ -12,27 +12,36 @@ zero_image = zero_cross(image);
 
 figure();
 imshow(canny_image);
+title("Canny");
 
 figure();
 imshow(laplacian_of_gaussian_image);
+title("Laplacian of Gaussian");
 
 figure();
 imshow(laplacian_4_image);
+title("Laplacian 4");
 
 figure();
 imshow(laplacian_8_image);
+title("Laplacian 8");
 
 figure();
 imshow(morphological_gradient_image);
+title("Morphological Gradient");
 
 figure();
 imshow(prewitt_image);
+title("Prewitt");
 
 figure();
 imshow(roberts_image);
+title("Roberts");
 
 figure();
 imshow(sobel_image);
+title("Sobel");
 
 figure();
 imshow(zero_image);
+title("Zero Crossing");
